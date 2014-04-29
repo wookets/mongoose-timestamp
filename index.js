13dd41ff5ca543c2037f269173d3a086d2110500
Sean Wesenberg <wookets@gmail.com>
2014-04-29 14:01:18 -0500

    0.2.0 - Added trashed timestamp and prop
    Signed-off-by: Sean Wesenberg <wookets@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 
 module.exports = function(schema, options) {
   schema.add({createdOn: {type: Date, default: Date.now, required: true}});
-  schema.add({modifiedOn: {type: Date, default: Date.now, required: true}});
+  schema.add({updatedOn: {type: Date, default: Date.now, required: true}});
+  schema.add({trashedOn: {type: Date}});
+  schema.add({trashed: {type: Boolean, default: false, required: true}});
   schema.pre('save', function(next) {
     if (this.createdOn == null) {
       this.createdOn = new Date();
     }
-    this.modifiedOn = new Date();
+    this.updatedOn = new Date();
+    if (this.isModified('trashed')) {
+      if (this.trashed) {
+        this.trashedOn = new Date();
+      } else {
+        this.trashedOn = undefined;
+      }
+    }
     next();
   });
-}
+};
